3ac6e286b12781bbf17d39f84e090b19b4cd33ac
yodepapa <45843277+yodepapa@users.noreply.github.com>
2020-12-18 17:30:49 +0800

    tcn.py文件注释添加

--- TCN/tcn.py
+++ TCN/tcn.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
 
-
+#为保证各隐藏层的单元数量和输入层单元数量一致，需要在序列两端padding一些零，右端就会产生不必要的卷积结果，需要砍掉。
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
         super(Chomp1d, self).__init__()
@@ -11,12 +11,13 @@ class Chomp1d(nn.Module):
     def forward(self, x):
         return x[:, :, :-self.chomp_size].contiguous()
 
-
+#先不扩张，两个卷积层卷一下 同一个dilation为什么搞两个卷积层啊？
 class TemporalBlock(nn.Module):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
+        #砍掉右端（序列末尾）多余的padding个数的卷积结果。
         self.chomp1 = Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
@@ -29,6 +30,7 @@ class TemporalBlock(nn.Module):
 
         self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
                                  self.conv2, self.chomp2, self.relu2, self.dropout2)
+        #当输入输出通道数不相等时需要再用个卷积层降采样
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
         self.relu = nn.ReLU()
         self.init_weights()
@@ -42,16 +44,19 @@ class TemporalBlock(nn.Module):
     def forward(self, x):
         out = self.net(x)
         res = x if self.downsample is None else self.downsample(x)
-        return self.relu(out + res)
+        return self.relu(out + res)   #一个残差
 
 
 class TemporalConvNet(nn.Module):
+    #num_channels是个数组
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
         super(TemporalConvNet, self).__init__()
         layers = []
+        #num_channels数组长度就是网络要拼接TemporalBlock的数量。
         num_levels = len(num_channels)
         for i in range(num_levels):
             dilation_size = 2 ** i
+            #确定各卷积层的通道数
             in_channels = num_inputs if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
